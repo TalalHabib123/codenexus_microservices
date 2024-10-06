1659b157d3744936d7c2a21891a70937e2277cc4
TalalHabib123 <talalhabib101@gmail.com>
2024-10-06 22:12:19 +0500

    Baseline for the SQS established

--- api-gateway/app/main.py
+++ api-gateway/app/main.py
@@ -2,14 +2,18 @@ import os
 import uvicorn
 from fastapi import FastAPI
 from app.api.endpoints.detection import detecton_gateway_router
+# from app.api.endpoints.websockets import websocket_gateway_router, lifespan   #Sockets Code For Iteration 2
 
-app = FastAPI()
+
+app = FastAPI() # lifespan=lifespan #Sockets Code For Iteration 2
 
 app.include_router(detecton_gateway_router, prefix="/detection")
+# app.include_router(websocket_gateway_router, prefix="/websockets")    #Sockets Code For Iteration 2
 
 @app.get("/")
 def health_check():
     return {"status": "ok"}
 
+        
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True)

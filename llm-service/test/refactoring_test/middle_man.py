aa07ee92b265617e306a2a562a7515dd5c0cfe2b
TalalHabib123 <talalhabib101@gmail.com>
2025-03-20 11:31:02 +0500

    Final 3 Refactoring (Testing Remaining)

---
+++ llm-service/test/refactoring_test/middle_man.py
@@ -0,0 +1,56 @@
+import boto3
+import os
+import json
+from dotenv import load_dotenv
+from utils_helper import get_or_create_queue
+
+
+load_dotenv()
+
+sqs = boto3.client(
+    'sqs', 
+    endpoint_url=str(os.getenv('SQS_ENDPOINT_URL')), 
+    region_name=str(os.getenv('REGION_NAME')),
+    aws_access_key_id=str(os.getenv('AWS_ACCESS_KEY_ID')),
+    aws_secret_access_key=str(os.getenv('AWS_SECRET_ACCESS_KEY'))
+)
+
+task_queue_url = get_or_create_queue(sqs, os.getenv('TASK_QUEUE_NAME'))
+response_queue_url = get_or_create_queue(sqs, os.getenv('RESPONSE_QUEUE_NAME'))
+
+GOD_Function = {
+    "file_path": "car.py",
+    "code_snippet": """
+
+    """,
+}
+
+message = { 
+    'correlation_id': '1234',
+    'task_type': 'refactoring',
+    'task_data': GOD_Function,
+    'task_job': 'middle_man'
+}
+
+sqs.send_message(QueueUrl=task_queue_url, MessageBody=json.dumps(message))
+
+while True:
+    response = sqs.receive_message(QueueUrl=response_queue_url, MaxNumberOfMessages=1, WaitTimeSeconds=5)
+    messages = response.get('Messages', [])
+    
+    if messages:
+        message_body = json.loads(messages[0]['Body'])
+        correlation_id = message_body['correlation_id']
+        processed_data = message_body['processed_data']
+        task_status = message_body['task_status']
+        
+        if task_status == "success":
+            print(f"Processed data for correlation ID {correlation_id}: {processed_data['refactored_code']}")
+        else:
+            print(f"Error processing data for correlation ID {correlation_id}")
+        
+        
+        receipt_handle = messages[0]['ReceiptHandle']
+        sqs.delete_message(QueueUrl=response_queue_url, ReceiptHandle=receipt_handle)
+        
+        break

3b66d8ac26c3c391f696bf3519c7afcc169e3404
malaikaaa <malaikazafar2003@gmail.com>
2025-03-21 07:02:02 +0500

    updates

--- web-service/mongo_models/Project.js
+++ web-service/mongo_models/Project.js
@@ -7,8 +7,7 @@ const ProjectSchema = new mongoose.Schema({
         required: true
     },
     description: {
-        type: String,
-        required: true
+        type: String
     },
     Owner: {
         type: mongoose.Schema.Types.ObjectId,
--- web-service/routes/scan.route.js
+++ web-service/routes/scan.route.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const scanController = require('../controller/scan.controller');
-
+const {vscodeAuth} = require('../middleware/auth.middleware')
 const router = express.Router();
 
 
-router.post('/addDetection', scanController.addDetection);
-router.post('/addRefactor', scanController.addRefactor);
+router.post('/addDetection',vscodeAuth, scanController.addDetection);
+router.post('/addRefactor', vscodeAuth, scanController.addRefactor);
 router.get('/daily', scanController.getDailyScans);
 router.get('/weekly', scanController.getWeeklyScans);
 router.get('/monthly', scanController.getMonthlyScans);
